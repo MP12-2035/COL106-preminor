479e3e4ee9a57d1d9792ca2295cdd3e54da65758
Samridhi Roy <66419090+MP12-2035@users.noreply.github.com>
2025-09-08 20:47:15 +0530

    FileSystem.hpp

---
+++ FileSystem.hpp
@@ -0,0 +1,32 @@
+#ifndef FILESYSTEM_HPP
+#define FILESYSTEM_HPP
+
+#include <string>
+#include "File.hpp"
+#include "HashMap.hpp"
+#include "Heap.hpp"
+
+class FileSystem {
+public:
+    FileSystem();
+    ~FileSystem();
+
+    void createFile(const std::string& filename);
+    void readFile(const std::string& filename);
+    void insertIntoFile(const std::string& filename, const std::string& content);
+    void updateFile(const std::string& filename, const std::string& content);
+    void snapshotFile(const std::string& filename, const std::string& message);
+    void rollbackFile(const std::string& filename, int version_id = -1);
+    void showHistory(const std::string& filename);
+    void recentFiles(int num);
+    void biggestTrees(int num);
+
+private:
+    HashMap<std::string, File*> files_map;
+    Heap recent_files_heap;
+    Heap biggest_trees_heap;
+
+    // Additional helper methods
+};
+
+#endif // FILESYSTEM_HPP
